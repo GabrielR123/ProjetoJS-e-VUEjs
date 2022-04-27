d1146137a7aa3aa6412a21fa3e46291112634dd0
Gabriel Ribeiro <gabrielribeiro969@gmail.com>
2022-04-27 09:33:11 -0300

    Adicionado CSS

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
-function rand(min = 1000, max = 3000) {
-  const num = Math.random() * (max - min) + min;
-  return Math.floor(num);
-}
-//console.log(rand());
+function Calculadora() {
+  this.display = document.querySelector('.display');
 
-function f1(callback) {
-  setTimeout(function () {
-    console.log('f1');
-    if (callback) callback();
-  }, rand());
-}
+  this.inicia = () => {
+    this.capturaCliques();
+    this.capturaEnter();
+  };
 
-function f2(callback) {
-  setTimeout(function () {
-    console.log('f2');
-    if (callback) callback();
-  }, rand());
-}
-
-function f3(callback) {
-  setTimeout(function () {
-    console.log('f3');
-    if (callback) callback();
-  }, rand());
-}
-
-f1(function () {
-  f2(function () {
-    f3(function () {
-      console.log('Ola');
+  this.capturaEnter = () => {
+    document.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) {
+        this.realizaConta();
+      }
     });
-  });
-});
-//f2();
-//f3();
-//console.log('Ola');
+  };
+
+  this.capturaCliques = () => {
+    document.addEventListener('click', (event) => {
+      const el = event.target;
+      if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+      if (el.classList.contains('btn-clear')) this.clear();
+      if (el.classList.contains('btn-del')) this.del();
+      if (el.classList.contains('btn-eq')) this.realizaConta();
+    });
+  };
+
+  this.realizaConta = () => {
+    try {
+      const conta = eval(this.display.value);
+
+      if (!conta) {
+        alert('Conta inválida');
+        return;
+      }
+
+      this.display.value = conta;
+    } catch (e) {
+      alert('Conta inválida');
+      return;
+    }
+  };
+
+  this.addNumDisplay = (el) => {
+    this.display.value += el.innerText;
+    this.display.focus();
+  };
+
+  this.clear = () => (this.display.value = '');
+  this.del = () => (this.display.value = this.display.value.slice(0, -1));
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
